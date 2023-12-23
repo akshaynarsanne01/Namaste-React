7eac90a8c64fe93762214f4df1ddf043aafef109
Akkshay Narsanne <akshaynarsanne01@gmail.com>
2023-12-23 16:21:14 +0530

    Minor UI searchbar and all restaurants changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,7 +3,6 @@ import { restaurantLists } from "../utils/constants";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 
-
 const Body = () => {
   // first hook use state
   // it is like global variable in react
@@ -27,36 +26,47 @@ const Body = () => {
     <Shimmer />
   ) : (
     <div className="w-full h-full">
-      <input
-        className="border rounded-lg p-3 mr-2"
-        placeholder="  Search...."
-        value={searchText}
-        onChange={(e) => {
-          setSearchText(e.target.value);
-        }}
-      />
-      <button
-        className="p-2 border-spacing-2 border-2 rounded-lg border-slate-400 hover:bg-2 hover:scale-105"
-        type="submit"
-        onClick={() => {
-          const filteredList = restaurantList.filter((res) =>
-            res.data.name.toLowerCase().includes(searchText.toLowerCase())
-          );
-          setFilteredData(filteredList);
-        }}
-      >
-        Search
-      </button>
-      <div className="flex justify-center border-solid border-black">
+      <div className="flex justify-center m-4">
+        <input
+          className="border rounded-lg p-3 mr-2"
+          placeholder="  Search...."
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
         <button
+          className="p-2 border-spacing-2 border-2 rounded-lg border-slate-400 hover:bg-2 hover:scale-105"
+          type="submit"
+          onClick={() => {
+            const filteredList = restaurantList.filter((res) =>
+              res.data.name.toLowerCase().includes(searchText.toLowerCase())
+            );
+            setFilteredData(filteredList);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="flex justify-center space-x-2 border-solid border-black">
+        <button
+          className="border-2 border-gray-300 rounded-lg p-2"
           onClick={() => {
             const filteredRes = restaurantList.filter(
               (res) => res.data.avgRating > 4
             );
-            setRestaurantList(filteredRes);
+            setFilteredData(filteredRes);
           }}
         >
           Top Restaurant
+        </button>
+        <button
+          className="border-2 border-gray-300 rounded-lg p-2"
+          onClick={() => {
+            setFilteredData(restaurantList);
+          }}
+        >
+          All Restaurants
         </button>
       </div>
       <div className="flex flex-wrap justify-center p-1">
